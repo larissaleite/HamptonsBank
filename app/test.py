c63840fc97edefbc1a215c94f9be8b5bf65f17cc
larissaleite <larissa.leite@gmail.com>
2015-05-26 11:53:21 -0300

    Added test file

---
+++ app/test.py
@@ -0,0 +1,13 @@
+import unittest, views
+
+class FlaskTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = views.app.test_client()
+
+    def test_index(self):
+        rv = self.app.get('/')
+        assert 'Hamptons Bank' in rv.data
+
+if __name__ == '__main__':
+    unittest.main()
