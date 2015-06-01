7663f5231ccc2e9a4d0a31ce94bf3924dc9fec96
larissaleite <larissa.leite@gmail.com>
2015-06-01 18:12:48 -0300

    Update views run

--- app/views.py
+++ app/views.py
@@ -104,6 +104,4 @@ def credit_savings():
 
 if __name__ == '__main__':
 	app.secret_key = 'secret key'
-	#app.run(debug=True)
-	port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+	app.run(debug=True)
